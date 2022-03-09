64adb9a3686d7fe6f508d6d72442a75fef2676b8
Iván Castaño <hansen5815@gmail.com>
2022-03-09 20:09:36 +0100

    Ejercicio práctico entregable refactorizacion y GitHub

---
+++ FicheroAleatorioVentana/src/iVentanaDepart.java
@@ -0,0 +1,22 @@
+import java.awt.event.ActionEvent;
+
+public interface iVentanaDepart {
+
+	/**
+	 * @param e
+	 */
+	void modificacion(ActionEvent e);
+
+	/**
+	 * @param e
+	 */
+	void baja(ActionEvent e);
+
+	/**
+	 * @param e
+	 */
+	void alta(ActionEvent e);
+
+	void consultas(ActionEvent e);
+
+}
